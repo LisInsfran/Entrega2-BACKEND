0d101d59bfb4ef917973e12728c601e0af1b5585
Lis Insfran <tomlinumen@gmail.com>
2022-09-06 15:49:37 -0300

    Segunda entrega BACKEND

---
+++ main.js
@@ -0,0 +1,21 @@
+const Contenedor = require("./Contenedor");
+
+const contenedor = new Contenedor("productos.json");
+
+const main = async () => {
+  const id1 = await contenedor.save({ title: "Remera", price: 500 });
+  const id2 = await contenedor.save({ title: "Buzo", price: 600 });
+  const id3 = await contenedor.save({ title: "Pantalon", price: 400 });
+
+  console.log(id1, id2, id3); 
+
+  const object2 = await contenedor.getById(1);
+  console.log(object2); 
+
+  await contenedor.deleteById(1);
+
+  const allCurrentObjects = await contenedor.getAll();
+  console.log(allCurrentObjects);
+};
+
+main();
